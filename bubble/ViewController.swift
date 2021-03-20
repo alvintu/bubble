5b775e7472a2747d539eacc4eebd9fa8f7f2396e
Alvin <tu.alvinj@gmail.com>
2021-03-20 10:40:26 -0400

    Replace extraneous methods with configuration methods for viewmodel and ui setup

--- bubble/ViewController.swift
+++ bubble/ViewController.swift
@@ -23,52 +23,50 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-//        square.backgroundColor = UIColor.gray
-//        view.addSubview(square)
         
-        
-        let bubble1 = BubbleView(frame: CGRect(x: 40, y: 600, width: 100, height: 100    ))
-        view.addSubview(bubble1)
-        
-        let bubble2 = BubbleView(frame: CGRect(x: 140, y: 500, width: 100, height: 100    ))
-        view.addSubview(bubble2)
-
-        let bubble3 = BubbleView(frame: CGRect(x: 240, y: 600, width: 100, height: 100    ))
-        view.addSubview(bubble3)
-        
-        let bubble4 = BubbleView(frame: CGRect(x: 200, y: 300, width: 100, height: 100    ))
-        view.addSubview(bubble4)
-        let bubble5 = BubbleView(frame: CGRect(x: 50, y: 300, width: 100, height: 100    ))
-        view.addSubview(bubble5)
-        let bubble6 = BubbleView(frame: CGRect(x: 600, y: 500, width: 100, height: 100    ))
-        view.addSubview(bubble6)
-
-        let videos = setupModel()
-        data.append(contentsOf: videos)
-        bubble1.configure(with:data[0] )
-        bubble2.configure(with:data[1] )
-        bubble3.configure(with:data[2] )
-        bubble4.configure(with:data[3] )
-        bubble5.configure(with:data[4] )
-        bubble6.configure(with:data[5] )
-
-
         animator = UIDynamicAnimator(referenceView: view)
-        gravity = UIGravityBehavior(items: [bubble1, bubble2, bubble3, bubble4, bubble5, bubble6])
+        gravity = UIGravityBehavior(items: [])
         gravity.magnitude = 0.001
-    
         gravity.gravityDirection =  CGVector(dx: 0, dy: -0.5)
         animator.addBehavior(gravity)
         
-
-
-        collision = UICollisionBehavior(items: [bubble1, bubble2, bubble3, bubble4, bubble5, bubble6])
+        collision = UICollisionBehavior(items:[])
         collision.translatesReferenceBoundsIntoBoundary = true
         animator.addBehavior(collision)
+        let models = setupModel()
+        data.append(contentsOf: models)
+        
+        for model in data {
+            configureBubble(for: 80, and: model)
+        }
 
 
         // Do any additional setup after loading the view.
+    }
+    
+    func configureBubble(for size: CGFloat){
+        let randomX = CGFloat.random(in: self.view.bounds.minX ..< self.view.bounds.maxX)
+        let bubble = BubbleView(frame: CGRect(x: randomX, y: 500, width: size, height: size))
+        let video  = VideoModel(caption: "hunter in bath", username: "@alvin", audioTrackName: "not sure", videoFileName: "1", videoFileFormat: "mp4")
+        bubble.configure(with:video )
+        view.addSubview(bubble)
+
+        gravity.addItem(bubble)
+        collision.addItem(bubble)
+        
+
+    }
+    
+    func configureBubble(for size: CGFloat, and viewModel: VideoModel){
+        let randomX = CGFloat.random(in: self.view.bounds.minX ..< self.view.bounds.maxX)
+        let bubble = BubbleView(frame: CGRect(x: randomX, y: 500, width: size, height: size))
+        bubble.configure(with:viewModel )
+        view.addSubview(bubble)
+
+        gravity.addItem(bubble)
+        collision.addItem(bubble)
+        
+
     }
     
     
@@ -77,10 +75,14 @@ class ViewController: UIViewController {
            let model2 = VideoModel(caption: "hunter running in grass", username: "@alvin", audioTrackName: "not sure", videoFileName: "2", videoFileFormat: "mp4")
             let model3 = VideoModel(caption: "hunter on sofa", username: "@alvin", audioTrackName: "not sure", videoFileName: "3", videoFileFormat: "mp4")
             let model4 = VideoModel(caption: "hunter on sofa", username: "@alvin", audioTrackName: "not sure", videoFileName: "1", videoFileFormat: "mp4")
-            let model5 = VideoModel(caption: "hunter on sofa", username: "@alvin", audioTrackName: "not sure", videoFileName: "1", videoFileFormat: "mp4")
-            let model6 = VideoModel(caption: "hunter on sofa", username: "@alvin", audioTrackName: "not sure", videoFileName: "1", videoFileFormat: "mp4")
+            let model5 = VideoModel(caption: "hunter on sofa", username: "@alvin", audioTrackName: "not sure", videoFileName: "2", videoFileFormat: "mp4")
+            let model6 = VideoModel(caption: "hunter on sofa", username: "@alvin", audioTrackName: "not sure", videoFileName: "3", videoFileFormat: "mp4")
+            let model7 = VideoModel(caption: "hunter on sofa", username: "@alvin", audioTrackName: "not sure", videoFileName: "1", videoFileFormat: "mp4")
+            let model8 = VideoModel(caption: "hunter on sofa", username: "@alvin", audioTrackName: "not sure", videoFileName: "2", videoFileFormat: "mp4")
+            let model9 = VideoModel(caption: "hunter on sofa", username: "@alvin", audioTrackName: "not sure", videoFileName: "3", videoFileFormat: "mp4")
+
 //            let array = [model1,model2,model3]
-            let array = [model1,model2,model3, model4,model5,model6]
+            let array = [model1,model2,model3, model4,model5,model6, model7, model8, model9]
            print(array)
            return array
        }
